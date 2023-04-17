c19c7cc2f8b4e5f9aa2ab2093327df2440a06212
Murat Kaipov <mkkaipov@gmail.com>
2023-04-17 17:32:24 +0300

    solved sort_except_zero

---
+++ sort_except_zero.py
@@ -0,0 +1,26 @@
+from collections.abc import Iterable
+
+
+def except_zero(items: list[int]) -> Iterable[int]:
+    # your code here
+    res = sorted([i for i in items if i != 0])
+    for indx, elem in enumerate(items):
+        if  elem == 0:
+            res.insert(indx, elem)
+    return res
+
+
+
+
+
+print("Example:")
+print(list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])))
+
+# These "asserts" are used for self-checking
+assert list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])) == [1, 3, 0, 0, 4, 4, 5, 0, 7]
+assert list(except_zero([0, 2, 3, 1, 0, 4, 5])) == [0, 1, 2, 3, 0, 4, 5]
+assert list(except_zero([0, 0, 0, 1, 0])) == [0, 0, 0, 1, 0]
+assert list(except_zero([4, 5, 3, 1, 1])) == [1, 1, 3, 4, 5]
+assert list(except_zero([0, 0])) == [0, 0]
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
